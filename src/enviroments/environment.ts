64ea6c6b87163af1b637ea567774875427a43ecd
lbarragan <lbarragan@viamatica.com>
2023-04-25 21:10:10 -0500

    cambios  - agregado idiomas

--- src/enviroments/environment.ts
+++ src/enviroments/environment.ts
@@ -7,4 +7,41 @@ export const environment = {
   recaptcha: {
     siteKey: '6LfL3aAlAAAAAER5RAdwJQf7_-krF_y7R5FnnbpF',
   },
+  languages: [
+    {
+      label: 'Español',
+      alias: 'ec',
+      icon: 'ec.gif',
+    },
+    {
+      label: 'Ingles',
+      alias: 'en',
+      icon: 'en.gif',
+    },
+    {
+      label: 'Portugués Brasileño',
+      alias: 'br',
+      icon: 'br.gif',
+    },
+    {
+      label: 'Francés',
+      alias: 'fr',
+      icon: 'fr.gif',
+    },
+    {
+      label: 'Italiano',
+      alias: 'it',
+      icon: 'it.gif',
+    },
+    {
+      label: 'Mandarín',
+      alias: 'cn',
+      icon: 'cn.gif',
+    },
+    {
+      label: 'Hindi',
+      alias: 'in',
+      icon: 'in.gif',
+    },
+  ],
 };
